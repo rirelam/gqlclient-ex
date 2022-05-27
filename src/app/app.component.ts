eec1255b812499979fcc2743fa20997f7f21c52f
Ricardo Lamar Meneses <ricardo.lamar@gmail.com>
2022-05-27 14:38:23 -0400

    proyecto creado ejemplo query

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+import { User } from './models/user.model';
+
+
+const Get_Users = gql`
+query{
+  users{
+    userName,
+    userMail,
+    rolesForUser{
+      name
+    },
+  }
+}
+`;
+
+const Get_User = gql`
+query ($userMail:String){
+  user(userMail:$userMail){
+    userName,
+    userMail,
+    rolesForUser{
+      name
+    }
+  }
+}`;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'gql-apollo';
+export class AppComponent implements OnInit {
+  user?: User;
+  userMail: string = '';
+  loaded = false
+
+  constructor(private apollo: Apollo) { }
+
+  ngOnInit(): void {
+    // this.apollo.watchQuery<any>({
+    //   query: Get_Users
+    // })
+    //   .valueChanges
+    //   .subscribe(({ data, loading }) => {
+    //     console.log(loading);
+    //     this.allUsers = data.users;
+    //     console.log(JSON.stringify(this.allUsers));
+    //   })
+    return;
+  }
+
+  searchUser(){
+   this.apollo.watchQuery<any>({
+      query: Get_User,
+     variables: {
+        useMail: this.userMail
+      }
+    })
+    .valueChanges
+    .subscribe(({data, loading}) => {
+      console.log(loading);
+      this.user = data.user as User;
+      this.loaded = !!!this.user;
+    });
+  }
 }
